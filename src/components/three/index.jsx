f2cbe3f53554b8a8b7249189bd18188199b01d16
sifat <sifat0666@gmail.com>
2023-05-04 01:40:39 +0600

    light

--- src/components/three/index.jsx
+++ src/components/three/index.jsx
@@ -3,6 +3,7 @@ import { angleToRadians } from "../../utils";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
+import { useEffect } from "react";
 
 const Three = () => {
   const orbitControlRef = useRef(null);
@@ -10,10 +11,16 @@ const Three = () => {
     // const {x, y} = state.mouse
   });
 
+  useEffect(() => {
+    if (!!orbitControlRef.current) {
+      console.log(orbitControlRef.current);
+    }
+  }, [orbitControlRef.current]);
+
   return (
     <>
       <PerspectiveCamera makeDefault position={[0, 1, 7]} />
-      <OrbitControls />
+      <OrbitControls ref={orbitControlRef} />
       <mesh position={[0, 0.5, 0]}>
         <sphereGeometry args={[0.5, 32, 32]} />
         <meshStandardMaterial color="#ffffff" />
@@ -21,10 +28,12 @@ const Three = () => {
 
       <mesh rotation={[-angleToRadians(90), 0, 0]}>
         <planeGeometry args={[7, 7]} />
-        <meshStandardMaterial color="red" />
+        <meshStandardMaterial color="green" />
       </mesh>
 
-      <ambientLight args={["#ffffff", 1]} />
+      <ambientLight args={["#ffffff", 0.1]} />
+
+      <pointLight args={["#ffffff", 1]} position={[-3, 1, 0]} />
     </>
   );
 };
